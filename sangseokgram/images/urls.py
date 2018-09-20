af9007815d3b8a024e30609e192a9510afd3bece
ldss3sang <ldss3sang@gmail.com>
2018-09-19 22:50:47 -0400

    up to edit image

--- sangseokgram/images/urls.py
+++ sangseokgram/images/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
-    path("<int:image_id>/unlike/", view=views.UnLikeImage.as_view(), name="unlike_image"),
+    path("<int:image_id>/", view=views.ImageDetail.as_view(), name="image"),
+    path("<int:image_id>/likes/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/unlikes/", view=views.UnLikeImage.as_view(), name="unlike_image"),
     path("<int:image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("<int:image_id>/comments/<int:comment_id>/", view=views.ModerateComments.as_view(), name="comment_image"),
     path("comments/<int:comment_id>/", view=views.Comment.as_view(), name="comment"),
     path("search/", view=views.Search.as_view(), name="search"),
     # path("all",view=views.ListAllImages.as_view(),name="all_images"),
